eb36bcfe7311c2da03ce056a7739e0099444d817
MarusyaBG <105644241+MarusyaBG@users.noreply.github.com>
2022-06-06 20:22:19 +0300

    Update 1.php

--- 1.php
+++ 1.php
@@ -1,1 +1,14 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<?php
+	$name = "Василиса";
+	echo <h2>"Привет, " . $name . "." . "</h2><br>";
+	?>
+</body>
+</html>
